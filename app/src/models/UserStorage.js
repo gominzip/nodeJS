11124916a0a56c736c0c6a3322e10b183fbde6a5
gominzip <ahaminji@naver.com>
2024-04-11 02:34:54 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,17 +1,26 @@
 "use strict";
 
+const fs = require("fs").promises; // promise 부여
+
 class UserStorage {
-  // static 변수로 만들어주면 인스턴스 없이 클래스 자체로 변수 접근 가능
-  static #users = {
-    // direct로 외부에서 접근하지 못하도록 #을 이용해 은닉화 (private)
-    id: ["test1", "test2", "test3"],
-    psword: ["1234", "2345", "3456"],
-    name: ["도라에몽", "둥냥이", "삼다수"],
-  };
+  static #getUserInfo(data, id) {
+    // private한 변수나 메서드는 항상 맨위에 (컨벤션)
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // users의 모든 키값을 리스트로 저장 => [id, psword, name]
+    const userInfo = usersKeys.reduce((newUsers, info) => {
+      // 키를 순회하면서 해당 idx에 해당하는 info 저장
+      newUsers[info] = users[info][idx];
+      return newUsers;
+    }, {});
+
+    return userInfo;
+  }
 
   // 인자로 들어오는 field에 대한 정보만 반환하는 함수
-  static getUsers(...fields) {  // 메서드 역시 클래스 자체에서 접근하려면 static 필요
-    const users = this.#users;
+  static getUsers(...fields) {
+    // 메서드 역시 클래스 자체에서 접근하려면 static 필요
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       // 인자로 들어온 field값 순회
       if (users.hasOwnProperty(field)) {
@@ -23,23 +32,21 @@ class UserStorage {
     return newUsers;
   }
 
-  static getUserInfo(id){
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // users의 모든 키값을 리스트로 저장 => [id, psword, name]
-    const userInfo = usersKeys.reduce((newUsers, info)=>{ // 키를 순회하면서 해당 idx에 해당하는 info 저장
-      newUsers[info] = users[info][idx];
-      return newUsers;
-    }, {});
-    return userInfo;
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
   }
 
-  static save(userInfo){
-    const users = this.#users;
+  static save(userInfo) {
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
-    return {success: true};
+    return { success: true };
   }
 }
 
