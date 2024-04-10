4ba1724ddf8e01d1d9ceeed63284fa420a7df3cb
gominzip <ahaminji@naver.com>
2024-04-10 23:57:55 +0900

    프론트단 js 파일 생성

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))  // 정적경로 추가
 app.use("/", home);  // use -> 미들 웨어 등록해주는 메서드
 
 module.exports = app;
