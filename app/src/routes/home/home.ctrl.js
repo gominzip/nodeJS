57a7b84862c154272f77fe953865e9bda534f150
gominzip <ahaminji@naver.com>
2024-04-11 22:51:51 +0900

    상태코드 및 최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,15 +6,17 @@ const User = require("../../models/User");
 const output = {
   // view부분을 output으로 묶어서 가독성 좋게
   home: (req, res) => {
-    logger.info(`GET / 200 "홈 화면으로 이동"`);
+    logger.info(`GET / 304 "홈 화면으로 이동"`);
     res.render("home/index");
   },
+
   login: (req, res) => {
-    logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+    logger.info(`GET /login 304 "로그인 화면으로 이동"`);
     res.render("home/login");
   },
+
   register: (req, res) => {
-    logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+    logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
     res.render("home/register");
   },
 };
@@ -23,32 +25,44 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
-    if (response.err)
-      logger.error(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200, // 200: 정상, 300: 페이지 이동, 400: 클라이언트 실수, 500: 서버 실수
+    };
+    log(response, url);
+    return res.status(url.status).json(response);
   },
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    if (response.err)
-      logger.error(
-        `POST /register 200 Response: "success: ${response.success}, msg: ${response.err}"`
-      );
-    else
-      logger.info(
-        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 400 : 201, // 새로운 데이터 생성은 201, 만약 같은 아이디로 생성하려 했을때는 409, 그외 DB 조작 문제면 서버 이슈이므로 500
+    };
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 };
 
 module.exports = {
   output,
   process,
+};
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response: "${response.success} ${
+        response.msg || ""
+      }"`
+    );
+  } else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response: "${response.success} ${
+        response.msg || ""
+      }"`
+    );
+  }
 };
